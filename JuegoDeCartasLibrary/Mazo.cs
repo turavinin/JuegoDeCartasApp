bc1ed416cefc4bd022e27c8cc889ace6feddfd8b
turavinin <aturavinin@gmail.com>
2021-01-18 16:20:38 -0300

    start CrearMazo method

---
+++ JuegoDeCartasLibrary/Mazo.cs
@@ -0,0 +1,20 @@
+﻿using JuegoDeCartasLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoDeCartasLibrary
+{
+    public abstract class Mazo
+    {
+        public List<CartaJugableModel> mazoCompleto = new List<CartaJugableModel>();
+        public List<CartaJugableModel> cartasDisponibles = new List<CartaJugableModel>();
+        public List<CartaJugableModel> cartasDesechadas = new List<CartaJugableModel>();
+
+        public void CrearMazo()
+        {
+        }
+    }
+}
