51eccebda173c4cfc0eb0d7f1815f227a355191d
turavinin <aturavinin@gmail.com>
2021-01-18 16:16:41 -0300

    Create card model and enums

---
+++ JuegoDeCartasLibrary/Enum/Enum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoDeCartasLibrary.Enum
+{
+    public enum Palo
+    {
+        Picas,
+        Corazones,
+        Trebol,
+        Diamantes
+    }
+
+    public enum Valor
+    {
+        As, 
+        Uno,
+        Dos,
+        Tres,
+        Cuatro,
+        Cinco,
+        Seis,
+        Siete,
+        Ocho,
+        Nueve,
+        Jota,
+        Dama,
+        Rey
+    }
+}
---
+++ JuegoDeCartasLibrary/Model/CartaJugableModel.cs
@@ -0,0 +1,15 @@
+﻿using JuegoDeCartasLibrary.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoDeCartasLibrary.Model
+{
+    public class CartaJugableModel
+    {
+        public Palo PaloCarta { get; set; }
+        public Valor ValorCarta { get; set; }
+    }
+}
